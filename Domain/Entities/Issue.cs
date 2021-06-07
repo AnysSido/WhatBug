27e520788885835af2889349a420126d49b4a2ae
Anys <Anys.I.Sido@gmail.com>
2021-06-07 13:27:47 +0100

    - Add simple Issue domain entity

---
+++ Domain/Entities/Issue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatBug.Domain.Entities
+{
+    public class Issue
+    {
+        public int IssueId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
