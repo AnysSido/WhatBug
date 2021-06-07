a8a69a043ec48a821f735be9030e3caf2e0c03b4
Anys <Anys.I.Sido@gmail.com>
2021-06-07 12:35:53 +0100

    - Make WhatBugDbContext implement interface IWhatBugDbContext

--- Persistence/WhatBugDbContext.cs
+++ Persistence/WhatBugDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WhatBug.Application.Common.Interfaces;
 
 namespace WhatBug.Persistence
 {
-    public class WhatBugDbContext : IdentityDbContext
+    public class WhatBugDbContext : IdentityDbContext, IWhatBugDbContext
     {
         public WhatBugDbContext(DbContextOptions<WhatBugDbContext> options)
             : base(options)
