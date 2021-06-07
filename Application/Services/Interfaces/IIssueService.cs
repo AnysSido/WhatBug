f3b4db3c4ef8d9030323529bf329824f64075f15
Anys <Anys.I.Sido@gmail.com>
2021-06-07 13:57:52 +0100

    - Add IssueService and interface

---
+++ Application/Services/Interfaces/IIssueService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatBug.Application.Services.Interfaces
+{
+    public interface IIssueService
+    {
+        public void CreateIssue(string name, string description);
+    }
+}
---
+++ Application/Services/IssueService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Services.Interfaces;
+
+namespace WhatBug.Application.Services
+{
+    class IssueService : IIssueService
+    {
+        private readonly IWhatBugDbContext _context;
+
+        public IssueService(IWhatBugDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateIssue(string name, string description)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
