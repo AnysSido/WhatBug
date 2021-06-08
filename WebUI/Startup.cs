06c6b38976fed7b6e26ce7fbc37d939768838304
Anys <Anys.I.Sido@gmail.com>
2021-06-08 11:33:07 +0100

    - Add controller route mapping to startup

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -59,6 +59,9 @@ namespace WhatBug.WebUI
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
