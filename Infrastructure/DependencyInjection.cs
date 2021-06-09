c7672fe245042ec68fbc7af36bb10561d843bd94
Anys <Anys.I.Sido@gmail.com>
2021-06-09 20:41:28 +0100

    - Update Startup/DI wiring for new AppIdentityDbContext

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Infrastructure.Identity;
+
+namespace WhatBug.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("IdentityDatabase")));
+
+            return services;
+        }
+    }
+}
