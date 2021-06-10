987de2cb6a5edf62a9774eb4f4d29763f6f37c7d
Anys <Anys.I.Sido@gmail.com>
2021-06-10 14:10:47 +0100

    - Implement CurrentUserService in WebUI project

---
+++ WebUI/Services/CurrentUserService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+
+namespace WhatBug.WebUI.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            UserId = int.TryParse(httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier), out var id) ? id : 0;
+            IsAuthenticated = UserId > 0;
+        }
+
+        public int UserId { get; }
+        public bool IsAuthenticated { get; }
+    }
+}
