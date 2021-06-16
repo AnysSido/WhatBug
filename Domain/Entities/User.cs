73ad20196795a448f171ee7bb8334f7f0269fab7
Anys <Anys.I.Sido@gmail.com>
2021-06-16 13:10:23 +0100

    - Add User entity

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Domain.Common;
+
+namespace WhatBug.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+    }
+}
