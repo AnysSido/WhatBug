cee7fb168c88fb99463c9ce9880ff83f1e0c6373
Anys <Anys.I.Sido@gmail.com>
2021-06-16 13:10:46 +0100

    - Add UserClaim entity

---
+++ Application/Permissions/UserClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace WhatBug.Application.Permissions
+{
+    public class UserClaim
+    {
+        public int UserId { get; set; }
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+    }
+}
