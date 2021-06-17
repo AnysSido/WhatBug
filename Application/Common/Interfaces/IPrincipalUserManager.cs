8f530420e26ad8edad811f77042a3edc57017eb9
Anys <Anys.I.Sido@gmail.com>
2021-06-17 13:24:02 +0100

    - Add Create/Delete/SetUserId methods to PrincipalUserManager and interface

--- Application/Common/Interfaces/IPrincipalUserManager.cs
+++ Application/Common/Interfaces/IPrincipalUserManager.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhatBug.Application.Common.Models;
 
 namespace WhatBug.Application.Common.Interfaces
 {
     public interface IPrincipalUserManager
     {
-        public Task<bool> UsernameExists(string username);
+        Task<Result> CreateUserAsync(string username, string password);
+        Task<Result> DeleteUserAsync(string username);
+        Task<Result> SetUserId(string username, int userId);
     }
 }
--- Infrastructure/Identity/PrincipalUserManager.cs
+++ Infrastructure/Identity/PrincipalUserManager.cs
@@ -6,27 +6,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Common.Models;
 using WhatBug.Infrastructure.Identity;
 
 namespace Infrastructure.Identity
 {
     class PrincipalUserManager : IPrincipalUserManager
     {
-        private readonly AppIdentityDbContext _context;
         private readonly UserManager<PrincipalUser> _userManager;
 
-        public PrincipalUserManager(AppIdentityDbContext context, UserManager<PrincipalUser> userManager)
+        public PrincipalUserManager(UserManager<PrincipalUser> userManager)
         {
-            _context = context;
             _userManager = userManager;
         }
 
-        public async Task<bool> UsernameExists(string username)
+        public async Task<Result> CreateUserAsync(string username, string password)
         {
-            var normalizedUsername = _userManager.NormalizeName(username);
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.NormalizedUserName == normalizedUsername);
+            var user = new PrincipalUser()
+            {
+                UserName = username,
+                Email = username,
+            };
 
-            return user != null;
+            var result = await _userManager.CreateAsync(user, password);
+
+            return result.Succeeded ? Result.Success() : Result.Failure(result.Errors.Select(e => e.Description));
+        }
+
+        public async Task<Result> DeleteUserAsync(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user == null)
+                return Result.Failure(new string[] { $"User {username} not found." });
+
+            var result = await _userManager.DeleteAsync(user);
+
+            return result.Succeeded ? Result.Success() : Result.Failure(result.Errors.Select(e => e.Description));
+        }
+
+        public async Task<Result> SetUserId(string username, int userId)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            if (user == null)
+                return Result.Failure(new string[] { $"User {username} not found." });
+
+            user.UserId = userId;
+            var result = await _userManager.UpdateAsync(user);
+
+            return result.Succeeded ? Result.Success() : Result.Failure(result.Errors.Select(e => e.Description));
         }
     }
 }
