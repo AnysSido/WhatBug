97dce6c0024b139c50628982a31510532e1aaa5c
Anys <Anys.I.Sido@gmail.com>
2021-06-19 15:24:26 +0100

    - Update UserClaim fields

--- Application/Permissions/UserClaim.cs
+++ Application/Permissions/UserClaim.cs
@@ -1,9 +1,13 @@
-﻿namespace WhatBug.Application.Permissions
+﻿using WhatBug.Domain.Entities;
+
+namespace WhatBug.Application.Permissions
 {
     public class UserClaim
     {
+        public int UserClaimId { get; set; }
         public int UserId { get; set; }
         public string ClaimType { get; set; }
         public string ClaimValue { get; set; }
+        public User User { get; set; }
     }
 }
