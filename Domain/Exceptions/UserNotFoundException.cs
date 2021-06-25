9c7de40ddac472ea8e19a42e44c67ac82c7d0a3e
Anys <Anys.I.Sido@gmail.com>
2021-06-25 13:44:18 +0100

    - Add UserNotFoundException and InsufficientPermissionException

---
+++ Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WhatBug.Domain.Exceptions
+{
+    [Serializable]
+    public class UserNotFoundException : Exception
+    {
+        public int UserId { get; set; }
+
+        public UserNotFoundException() { }
+
+        public UserNotFoundException(string message)
+        : base(message) { }
+
+        public UserNotFoundException(string message, Exception inner)
+            : base(message, inner) { }
+
+        public UserNotFoundException(int userId)
+            : base($"User {userId} does not exist.") 
+        {
+            UserId = userId;
+        }
+
+        public UserNotFoundException(int userId, Exception inner)
+            : base($"User {userId} does not exist.", inner)
+        {
+            UserId = userId;
+        }
+    }
+}
