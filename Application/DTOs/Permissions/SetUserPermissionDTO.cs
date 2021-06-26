1ffa3fff6eabb84ed844e342a1445fd612452794
Anys <Anys.I.Sido@gmail.com>
2021-06-26 11:37:29 +0100

    - Add PermissionService with interface and DTOs

---
+++ Application/DTOs/Permissions/SetUserPermissionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Domain.Entities.Permissions;
+
+namespace WhatBug.Application.DTOs.Permissions
+{
+    public class SetUserPermissionDTO
+    {
+        public int UserId { get; set; }
+        public List<Permission> Permissions { get; set; }
+    }
+}
---
+++ Application/Services/Interfaces/IPermissionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Models;
+using WhatBug.Application.DTOs.Permissions;
+using WhatBug.Domain.Entities.Permissions;
+
+namespace WhatBug.Application.Services.Interfaces
+{
+    public interface IPermissionService
+    {
+        Task SetRolePermissions(SetRolePermissionsDTO setRolePermissionsDTO);
+        Task SetUserPermissions(SetUserPermissionDTO setUserGlobalPermissionsDTO);
+        Task SetUserProjectRole(SetUserProjectRoleDTO setUserProjectRoleDTO);
+        Task<bool> UserHasPermission(int userId, Permission permission);
+    }
+}
