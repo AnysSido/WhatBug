c3cfac40df5bf09ffecaa4018b2b1dbb7efacdd7
Anys <Anys.I.Sido@gmail.com>
2021-07-03 15:57:44 +0100

    - Add PermissionsController with globalindex view

---
+++ WebUI/Controllers/PermissionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatBug.Application.Services.Interfaces;
+using WhatBug.WebUI.ViewModels.Permissions;
+
+namespace WebUI.Controllers
+{
+    public class PermissionsController : Controller
+    {
+        private readonly IUserService _userService;
+        public PermissionsController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Permissions/Global")]
+        public async Task<IActionResult> Global()
+        {
+            var dtos = await _userService.GetAllUsersWithPermissions();
+            return View("~/Views/Permissions/GlobalIndex.cshtml", dtos);
+        }
+    }
+}
