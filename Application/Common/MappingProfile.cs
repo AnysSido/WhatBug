9431da4a535ee97c5df867bb01d7dd8188432ce3
Anys <Anys.I.Sido@gmail.com>
2021-07-03 11:20:09 +0100

    - Add MappingProfile

---
+++ Application/Common/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Application.Services;
+using WhatBug.Domain.Entities;
+using WhatBug.Domain.Entities.Permissions;
+
+namespace WhatBug.Application.Common
+{
+    class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<User, UserWithPermissionsDTO>()
+                .ForMember(
+                    dest => dest.Permissions,
+                    opt => opt.MapFrom(src => src.UserPermissions.Select(p => p.Permission)));
+            CreateMap<Permission, PermissionDTO>();
+        }
+    }
+}
