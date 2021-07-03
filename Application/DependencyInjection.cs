1b81d2c7ce7f37c0be5ac99f465108ed0d0a8309
Anys <Anys.I.Sido@gmail.com>
2021-07-03 11:19:35 +0100

    - Add AutoMapper to Application project

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -18,6 +18,7 @@ namespace WhatBug.Application
             services.AddScoped<IIssueService, IssueService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IPermissionService, PermissionService>();
+            services.AddAutoMapper(typeof(DependencyInjection));
 
             return services;
         }
