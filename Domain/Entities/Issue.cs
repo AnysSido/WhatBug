771844439bde68aaeab162a359c522a27950b16e
Anys <Anys.I.Sido@gmail.com>
2021-07-04 16:12:46 +0100

    - Make Issue auditable

--- Domain/Entities/Issue.cs
+++ Domain/Entities/Issue.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WhatBug.Domain.Common;
 
 namespace WhatBug.Domain.Entities
 {
-    public class Issue
+    public class Issue : AuditableEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
