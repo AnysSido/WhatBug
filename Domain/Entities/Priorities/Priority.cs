15348880bb0b6450ad4578452cb0ef32a042d6b5
Anys <Anys.I.Sido@gmail.com>
2021-07-05 11:58:52 +0100

    - Create Priority/PriorityScheme relationship + migration

--- Domain/Entities/Priorities/Priority.cs
+++ Domain/Entities/Priorities/Priority.cs
@@ -11,5 +11,7 @@ namespace WhatBug.Domain.Entities.Priorities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public List<PriorityScheme> PrioritySchemes { get; set; }
     }
 }
--- Domain/Entities/Priorities/PriorityScheme.cs
+++ Domain/Entities/Priorities/PriorityScheme.cs
@@ -13,6 +13,7 @@ namespace WhatBug.Domain.Entities.Priorities
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public List<Priority> Priorities { get; set; }
         public List<Project> Projects { get; set; }
     }
 }
