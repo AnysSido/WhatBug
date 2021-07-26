bca22c3b98f78ea18f726f7b89c8ba67bcd78c0a
Anys <Anys.I.Sido@gmail.com>
2021-07-26 11:20:41 +0100

    - Remove unused commented methods

--- WebUI/Controllers/ProjectsController.cs
+++ WebUI/Controllers/ProjectsController.cs
@@ -37,24 +37,6 @@ namespace WhatBug.WebUI.Controllers
             return View(await _projectService.ListProjects());
         }
 
-        // GET: Projects/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var project = await _context.Projects
-        //        .FirstOrDefaultAsync(m => m.ProjectId == id);
-        //    if (project == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(project);
-        //}
-
         [HttpGet]
         public async Task<IActionResult> Create()
         {
@@ -78,85 +60,5 @@ namespace WhatBug.WebUI.Controllers
             await _projectService.CreateProject(_mapper.Map<CreateProjectDTO>(vm));
             return RedirectToAction(nameof(Index));
         }
-
-        // GET: Projects/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var project = await _context.Projects.FindAsync(id);
-        //    if (project == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(project);
-        //}
-
-        // POST: Projects/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("ProjectId,Name,Description")] Project project)
-        //{
-        //    if (id != project.ProjectId)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(project);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!ProjectExists(project.ProjectId))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(project);
-        //}
-
-        // GET: Projects/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var project = await _context.Projects
-        //        .FirstOrDefaultAsync(m => m.ProjectId == id);
-        //    if (project == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(project);
-        //}
-
-        // POST: Projects/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var project = await _context.Projects.FindAsync(id);
-        //    _context.Projects.Remove(project);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
     }
 }
