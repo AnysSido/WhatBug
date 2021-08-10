d3f82a127b87a4212a143dae7ab327449d628775
Anys <Anys.I.Sido@gmail.com>
2021-08-10 10:27:01 +0100

    - Add AutoMapper to base controller

--- WebUI/Controllers/BaseController.cs
+++ WebUI/Controllers/BaseController.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
 
 namespace WhatBug.WebUI.Controllers
 {
     public abstract class BaseController : Controller
     {
         private IMediator _mediator;
+        private IMapper _mapper;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        protected IMapper Mapper => _mapper ??= HttpContext.RequestServices.GetService<IMapper>();
     }
 }
