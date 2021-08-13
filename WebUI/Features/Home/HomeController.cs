6b23427230bea9e5602485fac680acfec08b76b1
Anys <Anys.I.Sido@gmail.com>
2021-08-13 12:39:25 +0100

    - Move Home views and controller to features

--- WebUI/Features/Home/HomeController.cs
+++ WebUI/Features/Home/HomeController.cs
@@ -12,29 +12,12 @@ using WhatBug.Infrastructure.Identity;
 using WhatBug.WebUI.Routing;
 using WhatBug.WebUI.ViewModels;
 
-namespace WebUI.Controllers
+namespace WhatBug.WebUI.Features.Home
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly UserManager<PrincipalUser> _userManager;
-        private readonly SignInManager<PrincipalUser> _signInManager;
-
-        public HomeController(ILogger<HomeController> logger, UserManager<PrincipalUser> userManager, SignInManager<PrincipalUser> signInManager)
-        {
-            _logger = logger;
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
-
         public async Task<IActionResult> Index()
         {
-            var username = "test5@mytest.com";
-            var user = await _userManager.FindByNameAsync(username);
-            await _signInManager.SignInAsync(user, isPersistent: true);
-            //await AuthenticationHttpContextExtensions.SignOutAsync(HttpContext, "Identity.Application");
-            //await _signInManager.SignOutAsync();
-            var tmp = User.Claims.Select(c => new { c.Type, c.Value });
             return View();
         }
 
