86f1080a32e597f7aee5c802f3020c188e8bc0d0
Anys <Anys.I.Sido@gmail.com>
2021-08-15 21:36:03 +0100

    - Add Icon TagHelper to render icons from <icon> tags - Add Select2Option tag helper to render select options to work with custom select2 component adding icon functionality - Move ScriptTagHelper to tag helper namespace

---
+++ WebUI/TagHelpers/IconTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+using WhatBug.WebUI.Services.Interfaces;
+
+namespace WhatBug.WebUI.TagHelpers
+{
+    public class IconTagHelper : TagHelper
+    {
+        private readonly IIconService _iconService;
+
+        public string Icon { get; set; } // WhatBug icon name
+        public string Color { get; set; } // WhatBug color
+
+        public IconTagHelper(IIconService iconService)
+        {
+            _iconService = iconService;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "i";
+
+            var iconName = _iconService.IconNameToClass(Icon ?? string.Empty);
+            var iconColor = "wb-color-" + Color?.ToLower();
+
+            output.AddClass(iconColor, HtmlEncoder.Default);
+
+            foreach (var @class in iconName.Split())
+            {
+                output.AddClass(@class, HtmlEncoder.Default);
+            }
+        }
+    }
+}
---
+++ WebUI/TagHelpers/Select2OptionTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+using WhatBug.WebUI.Services.Interfaces;
+
+namespace WhatBug.WebUI.TagHelpers
+{
+    public class Select2OptionTagHelper : OptionTagHelper
+    {
+        private readonly IIconService _iconService;
+
+        public string Icon { get; set; }
+        public string IconColor { get; set; }
+
+        public Select2OptionTagHelper(IHtmlGenerator generator, IIconService iconService) : base(generator)
+        {
+            _iconService = iconService;
+        }        
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+            output.TagName = "option";
+
+            var iconName = _iconService.IconNameToClass(Icon ?? string.Empty);
+            var iconColor = "wb-color-" + IconColor?.ToLower();
+
+            output.Attributes.SetAttribute("data-icon", iconName);
+            output.Attributes.SetAttribute("data-icon-color", iconColor);
+        }
+    }
+}
