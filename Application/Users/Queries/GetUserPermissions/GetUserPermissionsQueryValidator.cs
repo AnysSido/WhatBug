6309af9af20cb3574f5d4cc6ac078499a05349e8
Anys <Anys.I.Sido@gmail.com>
2021-08-16 12:51:55 +0100

    - Refactor permission middleware to use CQRS feature slice

---
+++ Application/Users/Queries/GetUserPermissions/GetUserPermissionsQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace WhatBug.Application.Users.Queries.GetUserPermissions
+{
+    public class GetUserPermissionsQueryValidator : AbstractValidator<GetUserPermissionsQuery>
+    {
+        public GetUserPermissionsQueryValidator()
+        {
+            RuleFor(v => v.UserId).NotEmpty();
+        }
+    }
+}
