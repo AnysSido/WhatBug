49636869cffb7ae8bb3e6183b5b44bc472ef988b
Anys <Anys.I.Sido@gmail.com>
2021-08-20 11:29:40 +0100

    WBUG-8 Remove loading of usernames from authprovider now that they are available on user entities

--- Application/Permissions/Queries/GetGlobalPermissions/GetGlobalPermissionsQueryHandler.cs
+++ Application/Permissions/Queries/GetGlobalPermissions/GetGlobalPermissionsQueryHandler.cs
@@ -10,14 +10,12 @@ namespace WhatBug.Application.Permissions.Queries.GetGlobalPermissions
     public class GetGlobalPermissionsQueryHandler : IRequestHandler<GetGlobalPermissionsQuery, GlobalPermissionsDTO>
     {
         private readonly IWhatBugDbContext _context;
-        private readonly IAuthenticationProvider _authProvider;
         private readonly IMapper _mapper;
 
-        public GetGlobalPermissionsQueryHandler(IWhatBugDbContext context, IMapper mapper, IAuthenticationProvider authProvider)
+        public GetGlobalPermissionsQueryHandler(IWhatBugDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
-            _authProvider = authProvider;
         }
 
         public async Task<GlobalPermissionsDTO> Handle(GetGlobalPermissionsQuery request, CancellationToken cancellationToken)
@@ -26,14 +24,6 @@ namespace WhatBug.Application.Permissions.Queries.GetGlobalPermissions
             {
                 Users = await _mapper.ProjectTo<UserDTO>(_context.Users).ToListAsync()
             };
-
-            // TODO: Fix this. We should not be querying usernames one by one. This should be replaced with data
-            // from the User table but right now the User table is empty so we are using username from the
-            // authentication table.
-            foreach (var user in dto.Users)
-            {
-                user.Username = await _authProvider.GetUsername(user.Id);
-            }
 
             return dto;
         }
