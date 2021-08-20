3bbd868fe1643071ae0d4512730948aaca9ff811
Anys <Anys.I.Sido@gmail.com>
2021-08-20 10:52:56 +0100

    WBUG-16 Add logout action

--- Application/Common/Interfaces/IAuthenticationProvider.cs
+++ Application/Common/Interfaces/IAuthenticationProvider.cs
@@ -7,5 +7,6 @@ namespace WhatBug.Application.Common.Interfaces
         Task<bool> CreateUserAsync(string username, string password, string email, int id);
         Task<string> GetUsername(int userId);
         Task<bool> SignInAsync(string username, string password, bool rememberMe);
+        Task SignOutAsync();
     }
 }
