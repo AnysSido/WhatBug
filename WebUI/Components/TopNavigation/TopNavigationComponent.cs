ccad98c84b685dd57d56c20a20f710cc6a5a0b05
Anys <Anys.I.Sido@gmail.com>
2021-08-20 11:01:17 +0100

    WBUG-14 Add user info widgets for main layout page showing the logged in user profile photo and name, plus option to view profile or logout

---
+++ WebUI/Components/TopNavigation/TopNavigationComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+
+namespace WhatBug.WebUI.Components.TopNavigation
+{
+    [ViewComponent(Name = "TopNavigation")]
+    public class TopNavigationComponent : ViewComponent
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public TopNavigationComponent(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var vm = new TopNavigationComponentViewModel
+            {
+                UserLoggedIn = _currentUserService.IsAuthenticated,
+                UserEmail = _currentUserService.Email,
+                UserFullName = $"{_currentUserService.FirstName} {_currentUserService.Surname}"
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ WebUI/Components/UserInfo/UserInfoComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+
+namespace WhatBug.WebUI.Components.UserInfo
+{
+    [ViewComponent(Name = "UserInfo")]
+    public class UserInfoComponent : ViewComponent
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public UserInfoComponent(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var vm = new UserInfoViewModel
+            {
+                Username = _currentUserService.Username,
+                Email = _currentUserService.Email
+            };
+
+            return View(vm);
+        }
+    }
+}
