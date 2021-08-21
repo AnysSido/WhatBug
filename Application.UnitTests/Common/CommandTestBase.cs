c4b558002e0389b4093f5f84ffd6aafae65067e4
Anys <Anys.I.Sido@gmail.com>
2021-08-21 17:32:56 +0100

    WBUG-20 Create context factory for tests and command base class

---
+++ Application.UnitTests/Common/CommandTestBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WhatBug.Application.UnitTests.Common;
+using WhatBug.Persistence;
+
+namespace Application.UnitTests.Common
+{
+    public class CommandTestBase : IDisposable
+    {
+        protected readonly WhatBugDbContext _context;
+        protected readonly WhatBugContextFactory _factory;
+
+        public CommandTestBase()
+        {
+            _factory = new WhatBugContextFactory();
+            _context = _factory.Create();
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+            _factory.Dispose();
+        }
+    }
+}
---
+++ Application.UnitTests/Common/WhatBugContextFactory.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Data.Common;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Persistence;
+
+namespace WhatBug.Application.UnitTests.Common
+{
+    public class WhatBugContextFactory : IDisposable
+    {
+        private DbConnection _connection;
+        private DbContextOptions<WhatBugDbContext> _options;
+        private ICurrentUserService _currentUserService;
+
+        public WhatBugDbContext Create()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+
+                _options = new DbContextOptionsBuilder<WhatBugDbContext>()
+                .UseSqlite(_connection).Options;
+
+                var mockUserService = new Mock<ICurrentUserService>();
+                mockUserService.SetupGet(x => x.Id).Returns(1);
+                _currentUserService = mockUserService.Object;
+
+                using (var context = new WhatBugDbContext(_options))
+                {
+                    context.Database.EnsureCreated();
+                }
+            }
+
+            return new WhatBugDbContext(_options, _currentUserService);
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
