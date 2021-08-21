b1d0feeb89ada82266247af1cbecf926c8a5cf18
Anys <Anys.I.Sido@gmail.com>
2021-08-21 17:30:51 +0100

    WBUG-20 Refactor Result class to use new Error class for better organisation of errors

---
+++ Application/Common/Result/Errors.cs
@@ -0,0 +1,12 @@
+﻿namespace WhatBug.Application.Common.Result
+{
+    public static class Errors
+    {
+        public static class PermissionScheme
+        {
+            public static Error NameIsTaken(string schemeName) =>
+                new Error("SchemeNameIsTaken", $"A permission scheme with the name {schemeName} already exists");
+        }
+    }
+
+}
