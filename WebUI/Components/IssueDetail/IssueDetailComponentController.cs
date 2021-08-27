d4d5894cd4720acbd6c3caaafcc2a8361f73f059
Anys <Anys.I.Sido@gmail.com>
2021-08-27 15:53:10 +0100

    WBUG-3 Implement comments on issue detail page and rewrite the general structure of the component modal

--- WebUI/Components/IssueDetail/IssueDetailComponentController.cs
+++ WebUI/Components/IssueDetail/IssueDetailComponentController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Issues.Commands.AddComment;
 using WhatBug.Application.Issues.Commands.SetIssueDescription;
+using WhatBug.Application.Issues.Queries.GetComments;
 using WhatBug.Application.Issues.Queries.GetIssueDetail;
 using WhatBug.WebUI.Common;
 
@@ -8,6 +11,13 @@ namespace WhatBug.WebUI.Components.IssueDetail
 {
     public class IssueDetailComponentController : BaseController
     {
+        private readonly ICurrentUserService _currentUserService;
+
+        public IssueDetailComponentController(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
         public async Task<IActionResult> GetComponent(string issueId)
         {
             var dto = await Mediator.Send(new GetIssueDetailQuery { IssueId = issueId });
@@ -19,6 +29,20 @@ namespace WhatBug.WebUI.Components.IssueDetail
         {
             await Mediator.Send(new SetIssueDescriptionCommand { IssueId = issueId, Description = description });
             return Json(new { success = true });
+        }
+
+        public async Task<IActionResult> AddComment(string issueId, string comment)
+        {
+            await Mediator.Send(new AddCommentCommand { IssueId = issueId, Content = comment, AuthorId = _currentUserService.Id });
+            return Json(new { success = true });
+        }
+
+        public async Task<IActionResult> GetCommentsPartial(string issueId)
+        {
+            var dto = await Mediator.Send(new GetCommentsQuery { IssueId = issueId });
+            var vm = Mapper.Map<CommentsViewModel>(dto);
+
+            return PartialView("/Components/IssueDetail/_Comments.cshtml", vm);
         }
     }
 }
--- WebUI/wwwroot/js/components/IssueDetailComponent.js
+++ WebUI/wwwroot/js/components/IssueDetailComponent.js
@@ -5,7 +5,7 @@
 class IssueDetailComponent {
     constructor(issueId) {
         $.get('/issuedetailcomponent/getcomponent', {issueId: issueId}).done((modal) => {
-            this.issueDetailComponent = $('<div id="CreateIssueComponent" class="modal fade"></div>')
+            this.issueDetailComponent = $('<div id="IssueDetailComponent" class="modal fade"></div>')
             this.#BuildComponent(modal);
             this.issueDetailComponent.modal('show');
         });
@@ -14,17 +14,44 @@ class IssueDetailComponent {
     #BuildComponent(modal) {
         this.issueDetailComponent.html(modal);
         this.#SetVars();
-        this.#LoadQuill();        
+        this.#RegisterEvents();
+        this.#LoadComments();
+        this.#LoadQuill();
     }
 
     #SetVars = () => {
         // Main modal
-        this.issueDetailModal = this.issueDetailComponent.find('.createIssueModel');
+        this.issueDetailModal = this.issueDetailComponent.find('.issueDetailModal');
 
         // Data
         this.issueId = this.issueDetailComponent.find('.issueId');
         this.issueDescription = this.issueDetailComponent.find('.issueDescription')
+
+        // Comments
+        this.commentForm = this.issueDetailComponent.find('.commentForm');
+        this.commentContent = this.commentForm.find('.commentContent');
+        this.commentsContainer =  this.issueDetailComponent.find('.issueComments');
     }
+
+    #RegisterEvents = () => {
+        this.commentForm.submit((e) => {
+            e.preventDefault();          
+            $.post('issuedetailcomponent/addcomment', { issueId: this.issueId.val(), comment: this.commentContent.val() })
+            .done((result) => {
+                if (result.success) {
+                    this.commentContent.val("");
+                    this.#LoadComments();
+                }
+            });
+        })
+    };
+
+    #LoadComments = () => {
+        $.get('issuedetailcomponent/getcommentspartial', { issueId: this.issueId.val() })
+            .done((resp) => {
+                this.commentsContainer.html(resp);
+            });
+    };
 
     #LoadQuill = () => {
         this.quill = new QuillEditorComponent({
@@ -39,9 +66,5 @@ class IssueDetailComponent {
                 description: this.issueDescription.val(), 
             });
         });
-
-        // new Select2Component({
-        //     container: this.selectLists
-        // });
     }
 }
