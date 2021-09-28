412e7f94e7cd4bdff865100bc856902dd053a944
Anys <Anys.I.Sido@gmail.com>
2021-09-28 23:34:05 +0100

    WBUG-33 Add analysis tests to ensure command/query authorization is configured correctly

---
+++ Application.UnitTests/Analysis/AnalysisTests.cs
@@ -0,0 +1,65 @@
+﻿using Shouldly;
+using System.Linq;
+using System.Reflection;
+using WhatBug.Application.Common.MediatR;
+using WhatBug.Application.Common.Security;
+using WhatBug.Domain.Data;
+using WhatBug.Domain.Entities;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Analysis
+{
+    public class AnalysisTests
+    {
+        [Fact]
+        public void IQuery_RequiresAuthorizeAttribute()
+        {
+            var assembly = Assembly.GetAssembly(typeof(IQuery<>));
+
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IQuery<>)))
+                .ToList();
+
+            var typesMissingAttribute = types.Any(t => t.GetCustomAttribute<AuthorizeAttribute>() == null);
+
+            typesMissingAttribute.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ICommand_RequiresAuthorizeAttribute()
+        {
+            var assembly = Assembly.GetAssembly(typeof(ICommand<>));
+
+            var types = assembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ICommand<>)))
+                .ToList();
+
+            var typesMissingAttribute = types.Any(t => t.GetCustomAttribute<AuthorizeAttribute>() == null);
+
+            typesMissingAttribute.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void AuthorizeAttribute_WhenGivenProjectPermission_RequiresIRequireProjectAuthorization()
+        {
+            var assembly = Assembly.GetAssembly(typeof(IQuery<>));
+            var types = assembly.GetTypes().Where(t => t.GetCustomAttribute<AuthorizeAttribute>() != null);
+
+            foreach (var type in types)
+            {
+                var projectPermissions = type.GetCustomAttribute<AuthorizeAttribute>().Permissions
+                    .Select(p => Permissions.ToEntity(p))
+                    .Where(p => p.Type == PermissionType.Project);
+
+                if (projectPermissions.Any())
+                {
+                    var hasRequiredInterface = type.GetInterfaces().Contains(typeof(IRequireProjectAuthorization));
+
+                    hasRequiredInterface.ShouldBeTrue();
+                }
+            }
+        }
+    }
+}
