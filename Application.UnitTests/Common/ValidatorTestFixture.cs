7eaffa08a7af00485b3d64e70bb3671f8b506c37
Anys <Anys.I.Sido@gmail.com>
2021-10-03 12:54:52 +0100

    WBUG-20 Create ValidationTestFixture for validator tests with fixed dataset

---
+++ Application.UnitTests/Common/ValidatorTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WhatBug.Persistence;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Common
+{
+    public class ValidatorTestFixture : IDisposable
+    {
+        public WhatBugDbContext Context;
+
+        public ValidatorTestFixture()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var factory = new WhatBugContextFactory();
+
+            factory.CreateWithSeed(guid);
+            Context = factory.Create(guid);
+        }
+
+        public void Dispose()
+        {
+            WhatBugContextFactory.Dispose(Context);
+        }
+    }
+
+    [CollectionDefinition("ValidatorCollection")]
+    public class ValidatorCollection : ICollectionFixture<ValidatorTestFixture> { }
+}
