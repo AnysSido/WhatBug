d253477e3ef3d5510154d88735196c8ea1f82ef1
Anys <Anys.I.Sido@gmail.com>
2021-10-03 12:43:52 +0100

    WBUG-43 Add extension method to allow validation rules to throw exceptions

---
+++ Application/Common/Extensions/ValidationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+
+namespace WhatBug.Application.Common.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static IRuleBuilderOptions<T, TProperty> WithException<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule, Func<T, Exception> stateProvider)
+        {
+            return rule.WithState(x => stateProvider(x));
+        }
+    }
+}
