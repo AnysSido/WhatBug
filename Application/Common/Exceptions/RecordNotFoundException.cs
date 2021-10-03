7a14a1f80070ef4fc2b99cc73160f36ae3107ca0
Anys <Anys.I.Sido@gmail.com>
2021-10-03 12:44:09 +0100

    WBUG-43 Add RecordNotFoundException

---
+++ Application/Common/Exceptions/RecordNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WhatBug.Application.Common.Exceptions
+{
+
+    [Serializable]
+    public class RecordNotFoundException : Exception
+    {
+        public RecordNotFoundException() : base("Could not find record for the given key.") { }
+        public RecordNotFoundException(string message) : base(message) { }
+        public RecordNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected RecordNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
