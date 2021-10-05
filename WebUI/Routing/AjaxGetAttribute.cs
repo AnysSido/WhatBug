a3c92d54f71358331ea5d021ba085313db11f3f5
Anys <Anys.I.Sido@gmail.com>
2021-10-05 13:14:58 +0100

    WBUG-45 Add AjaxOnly attribute to reject non-ajax requests to controller actions

---
+++ WebUI/Routing/AjaxGetAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Routing;
+
+namespace WhatBug.WebUI.Routing
+{
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+        {
+            var request = routeContext.HttpContext.Request;
+
+            if (request == null)
+                return false;
+
+            if (request.Headers == null)
+                return false;
+
+            if (request.Headers["X-Requested-With"] != "XMLHttpRequest")
+                return false;
+
+            return true;
+        }
+    }
+}
