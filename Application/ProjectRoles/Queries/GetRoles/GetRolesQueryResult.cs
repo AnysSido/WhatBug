40264678b0461f7ca33a98e4ba4a4acb48746329
Anys <Anys.I.Sido@gmail.com>
2021-10-05 17:51:25 +0100

    WBUG-37 Refactor dto

--- Application/ProjectRoles/Queries/GetRoles/GetRolesQueryResult.cs
+++ Application/ProjectRoles/Queries/GetRoles/GetRolesQueryResult.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections.Generic;
+using WhatBug.Common.Mapping;
+using WhatBug.Domain.Entities;
 
 namespace WhatBug.Application.ProjectRoles.Queries.GetRoles
 {
     public class GetRolesQueryResult
     {
         public IList<RoleDto> Roles { get; set; }
+    }
+
+    public class RoleDto : IMapFrom<Role>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
