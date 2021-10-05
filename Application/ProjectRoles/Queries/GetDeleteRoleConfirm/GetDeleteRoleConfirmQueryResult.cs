d572495d4d8edf118446548a3861f4bb0f12f1d2
Anys <Anys.I.Sido@gmail.com>
2021-10-05 17:49:39 +0100

    WBUG-46 Refactor dto

--- Application/ProjectRoles/Queries/GetDeleteRoleConfirm/GetDeleteRoleConfirmQueryResult.cs
+++ Application/ProjectRoles/Queries/GetDeleteRoleConfirm/GetDeleteRoleConfirmQueryResult.cs
@@ -17,26 +17,26 @@ namespace WhatBug.Application.ProjectRoles.Queries.GetDeleteRole
         {
             profile.CreateMap<Role, GetDeleteRoleConfirmQueryResult>()
                 .ForMember(d => d.RoleId, opt => opt.MapFrom(s => s.Id))
-                .ForMember(d => d.ProjectsUsingRole, opt => opt.MapFrom(s =>s.ProjectUsers.Select(p => p.Project)));
+                .ForMember(d => d.ProjectsUsingRole, opt => opt.MapFrom(s => s.ProjectUsers.Select(p => p.Project)));
         }
+    }
 
-        public class ProjectDto : IMapFrom<Project>
+    public class ProjectDto : IMapFrom<Project>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<UserDto> Users { get; set; }
+
+        public void Mapping(Profile profile)
         {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public IList<UserDto> Users { get; set; }
-
-            public void Mapping(Profile profile)
-            {
-                profile.CreateMap<Project, ProjectDto>()
-                    .ForMember(d => d.Users, opt => opt.MapFrom(s => s.RoleUsers.Select(ru => ru.User)));
-            }
+            profile.CreateMap<Project, ProjectDto>()
+                .ForMember(d => d.Users, opt => opt.MapFrom(s => s.RoleUsers.Select(ru => ru.User)));
         }
+    }
 
-        public class UserDto : IMapFrom<User>
-        {
-            public string FirstName { get; set; }
-            public string Surname { get; set; }
-        }
+    public class UserDto : IMapFrom<User>
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
     }
 }
