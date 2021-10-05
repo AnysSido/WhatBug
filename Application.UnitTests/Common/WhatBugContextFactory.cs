6d8cec7acf070889192e6289701b898da8d4247d
Anys <Anys.I.Sido@gmail.com>
2021-10-05 16:32:11 +0100

    WBUG-20 Add additional seed data

--- Application.UnitTests/Common/WhatBugContextFactory.cs
+++ Application.UnitTests/Common/WhatBugContextFactory.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Moq;
+using System.Collections.Generic;
+using System.Linq;
 using WhatBug.Application.Common.Interfaces;
 using WhatBug.Domain.Entities;
 using WhatBug.Persistence;
@@ -37,7 +39,40 @@ namespace WhatBug.Application.UnitTests.Common
                 new Role { Id = 3, Name = "QA", Description = "QA Role"},
             });
 
+            context.Projects.AddRange(new[]
+            {
+                new Project { Id = 1, Name = "Test Project 1", Description = "Test Proj 1" },
+                new Project { Id = 2, Name = "Test Project 2", Description = "Test Proj 2" },
+                new Project { Id = 3, Name = "Test Project 3", Description = "Test Proj 3" }
+            });
+
+            context.Users.AddRange(new[]
+            {
+                new User {Id = 1, Username = "TestUser1", FirstName = "FirstName1", Surname = "Surname1", Email = "TestUser1@whatbug.com" },
+                new User {Id = 2, Username = "TestUser2", FirstName = "FirstName2", Surname = "Surname2", Email = "Test.User2@whatbug.com" },
+                new User {Id = 3, Username = "TestUser3", FirstName = "FirstName3", Surname = "Surname3", Email = "Test_User3@whatbug.com" },
+            });
+
             context.SaveChanges();
+
+            context.Projects.First().RoleUsers = new List<ProjectRoleUser>
+            {
+                new ProjectRoleUser { ProjectId = 1, RoleId = 1, UserId = 1 },
+                new ProjectRoleUser { ProjectId = 1, RoleId = 1, UserId = 2 },
+                new ProjectRoleUser { ProjectId = 1, RoleId = 1, UserId = 3 },
+                new ProjectRoleUser { ProjectId = 1, RoleId = 3, UserId = 3 },
+            };
+
+            context.Projects.Last().RoleUsers = new List<ProjectRoleUser>
+            {
+                new ProjectRoleUser { ProjectId = 3, RoleId = 1, UserId = 1 },
+                new ProjectRoleUser { ProjectId = 3, RoleId = 1, UserId = 2 },
+                new ProjectRoleUser { ProjectId = 3, RoleId = 3, UserId = 2 },
+                new ProjectRoleUser { ProjectId = 3, RoleId = 3, UserId = 3 },
+            };
+
+            context.SaveChanges();
+
             return context;
         }
 
