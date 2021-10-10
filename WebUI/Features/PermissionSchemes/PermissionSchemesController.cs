c5305d881122350b252736213b6f81242ddc9e0f
Anys <Anys.I.Sido@gmail.com>
2021-10-10 12:39:48 +0100

    WBUG-49 Clean up controller

--- WebUI/Features/PermissionSchemes/PermissionSchemesController.cs
+++ WebUI/Features/PermissionSchemes/PermissionSchemesController.cs
@@ -6,39 +6,52 @@ using WhatBug.Application.PermissionSchemes.Queries.GetCreatePermissionScheme;
 using WhatBug.Application.PermissionSchemes.Queries.GetGrantRolePermissions;
 using WhatBug.Application.PermissionSchemes.Queries.GetPermissionSchemes;
 using WhatBug.Application.PermissionSchemes.Queries.GetSchemeRoles;
+using WhatBug.Domain.Data;
+using WhatBug.WebUI.Authorization;
 using WhatBug.WebUI.Common;
 using WhatBug.WebUI.Features.PermissionSchemes.GrantRolePermissions;
+using WhatBug.WebUI.Routing;
 
 namespace WhatBug.WebUI.Features.PermissionSchemes
 {
+    [Route("admin/permission-schemes", Name = "PermissionSchemes")]
     public class PermissionSchemesController : BaseController
     {
+        [HttpGet("")]
+        [RequirePermission(Permissions.ManagePermissionSchemes)]
         public async Task<IActionResult> Index()
         {
             var dto = await Mediator.Send(new GetPermissionSchemesQuery());
-            return View(dto);
+            return View(dto.Result);
         }
 
-        [HttpGet]
+        [HttpGet("create", Name = "CreatePermissionScheme")]
+        [RequirePermission(Permissions.ManagePermissionSchemes)]
         public async Task<IActionResult> Create()
         {
             var dto = await Mediator.Send(new GetCreatePermissionSchemeQuery());
             return View(dto);
         }
 
-        [HttpPost]
+        [HttpPost("create", Name = "CreatePermissionScheme")]
+        [RequirePermission(Permissions.ManagePermissionSchemes)]
         public async Task<IActionResult> Create(CreatePermissionSchemeCommand command)
         {
             await Mediator.Send(command);
             return RedirectToAction(nameof(Index));
         }
 
+        [HttpGet("roles")]
+        [RequirePermission(Permissions.ManagePermissionSchemes)]
         public async Task<IActionResult> Roles(int schemeId)
         {
             var dto = await Mediator.Send(new GetSchemeRolesQuery { SchemeId = schemeId });
             return View(dto);
         }
 
+        [AjaxOnly]
+        [HttpGet("getgrantrolepermissionspartial")]
+        [RequirePermission(Permissions.ManagePermissionSchemes)]
         public async Task<IActionResult> GetGrantRolePermissionsPartial(int schemeId, int roleId)
         {
             var dto = await Mediator.Send(new GetGrantRolePermissionsQuery { SchemeId = schemeId, RoleId = roleId });
@@ -46,6 +59,8 @@ namespace WhatBug.WebUI.Features.PermissionSchemes
             return PartialView(vm);
         }
 
+        [HttpPost("grant", Name = "GrantRolePermissions")]
+        [RequirePermission(Permissions.ManagePermissionSchemes)]
         public async Task<IActionResult> GrantRolePermissions(GrantRolePermissionsViewModel vm)
         {
             var command = new GrantRolePermissionsCommand { SchemeId = vm.Id, RoleId = vm.RoleId, PermissionIds = vm.GetPermissionIds() };
