0c021e1bbd510da91823897541c7dbd282b0480c
Anys <Anys.I.Sido@gmail.com>
2021-10-13 19:10:12 +0100

    WBUG-49 Add delete command tests

---
+++ Application.UnitTests/PermissionSchemes/Queries/GetDeleteConfirm/GetDeleteConfirmQueryTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Shouldly;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.PermissionSchemes.Queries.GetDeleteConfirm;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.PermissionSchemes.Queries.GetDeleteConfirm
+{
+    [Collection("QueryCollection")]
+    public class GetDeleteConfirmQueryTests
+    {
+        private readonly IWhatBugDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetDeleteConfirmQueryTests(QueryTestFixture fixture)
+        {
+            _context = fixture.CreateContext();
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_ReturnsSchemeInfo()
+        {
+            // Arrange
+            var sut = new GetDeleteConfirmQueryHandler(_context, _mapper);
+
+            // Act
+            var result = await sut.Handle(new GetDeleteConfirmQuery { SchemeId = 2 }, CancellationToken.None);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            result.Result.SchemeId.ShouldBe(2);
+            result.Result.Name.ShouldBe("PermissionScheme2");
+            result.Result.Description.ShouldBe("PermissionSchemeDesc2");
+        }
+    }
+}
