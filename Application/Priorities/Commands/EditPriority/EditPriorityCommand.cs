9b415da575a714a70a6ecaead2a8cd5e50ba2edd
Anys <Anys.I.Sido@gmail.com>
2021-10-20 16:24:33 +0100

    WBUG-50 Refactor edit priority feature command

--- Application/Priorities/Commands/EditPriority/EditPriorityCommand.cs
+++ Application/Priorities/Commands/EditPriority/EditPriorityCommand.cs
@@ -1,18 +1,48 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
 using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Common.MediatR;
+using WhatBug.Application.Common.Security;
+using WhatBug.Domain.Data;
 
 namespace WhatBug.Application.Priorities.Commands.EditPriority
 {
-    public class EditPriorityCommand : IRequest
+    [Authorize(Permissions.ManagePriorities)]
+    public record EditPriorityCommand : ICommand<Response>
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int ColorId { get; set; }
         public int IconId { get; set; }
+    }
+
+    public class EditPriorityCommandHandler : IRequestHandler<EditPriorityCommand, Response>
+    {
+        private readonly IWhatBugDbContext _context;
+
+        public EditPriorityCommandHandler(IWhatBugDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response> Handle(EditPriorityCommand request, CancellationToken cancellationToken)
+        {
+            var priority = await _context.Priorities
+                .Include(p => p.Color)
+                .Include(p => p.Icon)
+                .FirstAsync(p => p.Id == request.Id);
+
+            priority.Name = request.Name;
+            priority.Description = !string.IsNullOrEmpty(request.Description) ? request.Description : null;
+            priority.Color = await _context.Colors.FirstAsync(c => c.Id == request.ColorId);
+            priority.Icon = await _context.Icons.FirstAsync(i => i.Id == request.IconId);
+
+            await _context.SaveChangesAsync();
+
+            return Response.Success();
+        }
     }
 }
--- Application/Priorities/Commands/EditPriority/EditPriorityCommandValidator.cs
+++ Application/Priorities/Commands/EditPriority/EditPriorityCommandValidator.cs
@@ -1,21 +1,61 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.Common.Extensions;
+using WhatBug.Application.Common.Interfaces;
 
 namespace WhatBug.Application.Priorities.Commands.EditPriority
 {
     public class EditPriorityCommandValidator : AbstractValidator<EditPriorityCommand>
     {
-        public EditPriorityCommandValidator()
+        private IWhatBugDbContext _context;
+
+        public EditPriorityCommandValidator(IWhatBugDbContext context)
         {
-            RuleFor(v => v.Id).NotEmpty();
-            RuleFor(v => v.Name).NotEmpty();
-            RuleFor(v => v.Description).NotEmpty();
-            RuleFor(v => v.ColorId).NotEmpty();
-            RuleFor(v => v.IconId).NotEmpty();
+            _context = context;
+
+            RuleFor(v => v.Id)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithException(cmd => new ArgumentException(nameof(cmd.Id)))
+                .MustAsync(IdExist).WithException(cmd => new RecordNotFoundException());
+
+            RuleFor(v => v.Name)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("Priority name cannot be empty")
+                .MustAsync(NameUnique).WithMessage(cmd => $"A priority with the name {cmd.Name} already exists");
+
+            RuleFor(v => v.ColorId)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithException(cmd => new ArgumentException(nameof(cmd.ColorId)))
+                .MustAsync(ColorExist).WithException(cmd => new RecordNotFoundException());
+
+            RuleFor(v => v.IconId)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithException(cmd => new ArgumentException(nameof(cmd.IconId)))
+                .MustAsync(IconExist).WithException(cmd => new RecordNotFoundException());
+        }
+
+        public async Task<bool> IdExist(EditPriorityCommand command, int id, CancellationToken cancellationToken)
+        {
+            return await _context.Priorities.AnyAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> NameUnique(EditPriorityCommand command, string name, CancellationToken cancellationToken)
+        {
+            return !await _context.Priorities.AnyAsync(p => p.Id != command.Id && p.Name == name);
+        }
+
+        public async Task<bool> ColorExist(EditPriorityCommand command, int colorId, CancellationToken cancellationToken)
+        {
+            return await _context.Colors.AnyAsync(c => c.Id == colorId);
+        }
+
+        public async Task<bool> IconExist(EditPriorityCommand command, int iconId, CancellationToken cancellationToken)
+        {
+            return await _context.Icons.AnyAsync(i => i.Id == iconId);
         }
     }
 }
