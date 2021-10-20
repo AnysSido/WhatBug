c252adf02feff4f13770351e278568666219c3cc
Anys <Anys.I.Sido@gmail.com>
2021-10-20 16:24:02 +0100

    WBUG-50 Add edit priority query tests

---
+++ Application.UnitTests/Priorities/Queries/GetEditPriority/GetEditPriorityQueryTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Priorities.Queries.GetEditPriority;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Queries.GetEditPriority
+{
+    [Collection("QueryCollection")]
+    public class GetEditPriorityQueryTests
+    {
+        private readonly IWhatBugDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetEditPriorityQueryTests(QueryTestFixture fixture)
+        {
+            _context = fixture.CreateContext();
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_ReturnsPriorityWithIconsAndColors()
+        {
+            // Arrange
+            var sut = new GetEditPriorityQueryHandler(_context, _mapper);
+
+            // Act
+            var result = await sut.Handle(new GetEditPriorityQuery { Id = 1 }, CancellationToken.None);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            result.Result.Id.ShouldBe(1);
+            result.Result.Name.ShouldBe("Priority1");
+            result.Result.Description.ShouldBe("PriorityDesc1");
+            result.Result.IconId.ShouldBe(1);
+            result.Result.ColorId.ShouldBe(1);
+            result.Result.Order.ShouldBe(4);
+            result.Result.Colors.Count.ShouldBe(3);
+            result.Result.Icons.Count.ShouldBe(3);
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_ReturnsColorsInOrder()
+        {
+            // Arrange
+            var sut = new GetEditPriorityQueryHandler(_context, _mapper);
+
+            // Act
+            var result = await sut.Handle(new GetEditPriorityQuery { Id = 1 }, CancellationToken.None);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            result.Result.Colors.Select(c => c.Id).ToArray().ShouldBeEquivalentTo(new[] { 1, 2, 3});
+        }
+    }
+}
---
+++ Application.UnitTests/Priorities/Queries/GetEditPriority/GetEditPriorityQueryValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.Priorities.Queries.GetEditPriority;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Queries.GetEditPriority
+{
+    [Collection("ValidatorCollection")]
+    public class GetEditPriorityQueryValidatorTests
+    {
+        private readonly GetEditPriorityQueryValidator _sut;
+
+        public GetEditPriorityQueryValidatorTests(ValidatorTestFixture fixture)
+        {
+            _sut = new GetEditPriorityQueryValidator(fixture.CreateContext());
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public async void Given_ZeroOrLowerPriorityId_HasArgumentException(int id)
+        {
+            // Arrange
+            var query = new GetEditPriorityQuery { Id = id };
+
+            // Act
+            var result = await _sut.TestValidateAsync(query);
+
+            // Assert
+            result.ShouldHaveExceptionFor(query => query.Id, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public async void Given_InvalidPriorityId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var query = new GetEditPriorityQuery { Id = 5 };
+
+            // Act
+            var result = await _sut.TestValidateAsync(query);
+
+            // Assert
+            result.ShouldHaveExceptionFor(query => query.Id, typeof(RecordNotFoundException));
+        }
+    }
+}
