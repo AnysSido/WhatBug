a7dcf98a10108895b22b13c5e6737c56b6cc1ea0
Anys <Anys.I.Sido@gmail.com>
2021-10-20 16:37:44 +0100

    WBUG-50 Add edit priority command tests

---
+++ Application.UnitTests/Priorities/Commands/EditPriorities/EditPriorityCommandTests.cs
@@ -0,0 +1,73 @@
+﻿using Application.UnitTests.Common;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Priorities.Commands.EditPriority;
+using WhatBug.Domain.Entities;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Commands.EditPriorities
+{
+    public class EditPriorityCommandTests : CommandTestBase
+    {
+        public EditPriorityCommandTests()
+        {
+            using (var context = CreateContext())
+            {
+                context.Priorities.AddRange(new[]
+                {
+                    new Priority { Id = 1, Name = "Name1", Description = "Desc1", Order = 1, ColorId = 1, IconId = 1 },
+                    new Priority { Id = 2, Name = "Name2", Description = "Desc2", Order = 2, ColorId = 2, IconId = 1 },
+                });
+                context.Colors.AddRange(new[]
+                {
+                    new Color { Id = 1, Name = "Color1" },
+                    new Color { Id = 2, Name = "Color2" },
+                });
+
+                context.Icons.Add(new Icon { Id = 1, Name = "Icon1", WebName = "icon1" });
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_UpdatesPriority()
+        {
+            // Arrange
+            var sut = new EditPriorityCommandHandler(_context);
+            var command = new EditPriorityCommand { Id = 1, Name = "NewName", Description = "NewDesc", IconId = 1, ColorId = 1 };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var priority = _context.Priorities.SingleOrDefault(p => p.Id == command.Id);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            priority.ShouldNotBeNull();
+            priority.Name.ShouldBe("NewName");
+            priority.Description.ShouldBe("NewDesc");
+            priority.IconId.ShouldBe(1);
+            priority.ColorId.ShouldBe(1);
+            priority.Order.ShouldBe(1);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task Handle_GivenNullOrEmptyDescription_SetsNullDescription(string description)
+        {
+            // Arrange
+            var sut = new EditPriorityCommandHandler(_context);
+            var command = new EditPriorityCommand { Id = 1, Name = "NewName", Description = description, IconId = 1, ColorId = 1 };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var priority = _context.Priorities.SingleOrDefault(p => p.Id == command.Id);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            priority.Description.ShouldBe(null);
+        }
+    }
+}
---
+++ Application.UnitTests/Priorities/Commands/EditPriorities/EditPriorityCommandValidatorTests.cs
@@ -0,0 +1,134 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.Priorities.Commands.EditPriority;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Commands.EditPriorities
+{
+    [Collection("ValidatorCollection")]
+    public class EditPriorityCommandValidatorTests
+    {
+        private EditPriorityCommandValidator _sut;
+
+        public EditPriorityCommandValidatorTests(ValidatorTestFixture fixture)
+        {
+            _sut = new EditPriorityCommandValidator(fixture.CreateContext());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void GivenNullOrEmptyName_HasValidationError(string name)
+        {
+            // Arrange
+            var command = new EditPriorityCommand { Name = name };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(command => command.Name)
+                .WithErrorMessage("Priority name cannot be empty");
+        }
+
+        [Fact]
+        public void Given_DuplicateName_HasValidationError()
+        {
+            // Arrange
+            var command = new EditPriorityCommand { Id = 1, Name = "Priority2" };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(command => command.Name)
+                .WithErrorMessage("A priority with the name Priority2 already exists");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Given_ZeroOrLowerPriorityId_HasArgumentException(int id)
+        {
+            // Arrange
+            var command = new EditPriorityCommand { Id = id };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.Id, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void Given_InvalidPriorityId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var command = new EditPriorityCommand { Id = 5 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.Id, typeof(RecordNotFoundException));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Given_ZeroOrLowerColorId_HasArgumentException(int id)
+        {
+            // Arrange
+            var command = new EditPriorityCommand { ColorId = id };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.ColorId, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void Given_InvalidColorId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var command = new EditPriorityCommand { ColorId = 4 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.ColorId, typeof(RecordNotFoundException));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Given_ZeroOrLowerIconId_HasArgumentException(int id)
+        {
+            // Arrange
+            var command = new EditPriorityCommand { IconId = id };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.IconId, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void Given_InvalidIconId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var command = new EditPriorityCommand { IconId = 4 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.IconId, typeof(RecordNotFoundException));
+        }
+    }
+}
