25a18e694d81789a6107c5abba5c337741bc8dcb
Anys <Anys.I.Sido@gmail.com>
2021-10-21 19:33:21 +0100

    WBUG-50 Refactor create priority feature command

--- Application/Priorities/Commands/CreatePriority/CreatePriorityCommand.cs
+++ Application/Priorities/Commands/CreatePriority/CreatePriorityCommand.cs
@@ -1,17 +1,52 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
 using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Common.MediatR;
+using WhatBug.Application.Common.Security;
+using WhatBug.Domain.Data;
+using WhatBug.Domain.Entities;
 
 namespace WhatBug.Application.Priorities.Commands.CreatePriority
 {
-    public class CreatePriorityCommand : IRequest
+    [Authorize(Permissions.ManagePriorities)]
+    public record CreatePriorityCommand : ICommand<Response<int>>
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public int ColorId { get; set; }
         public int IconId { get; set; }
+    }
+
+    public class CreatePriorityCommandHandler : IRequestHandler<CreatePriorityCommand, Response<int>>
+    {
+        private readonly IWhatBugDbContext _context;
+
+        public CreatePriorityCommandHandler(IWhatBugDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<int>> Handle(CreatePriorityCommand request, CancellationToken cancellationToken)
+        {
+            var color = await _context.Colors.FirstAsync(c => c.Id == request.ColorId);
+            var icon = await _context.Icons.FirstAsync(i => i.Id == request.IconId);
+            var order = await _context.Priorities.MaxAsync(p => (int?)p.Order) ?? 0;
+
+            var priority = new Priority
+            {
+                Name = request.Name,
+                Description = !string.IsNullOrEmpty(request.Description) ? request.Description : null,
+                Order = order + 1,
+                Color = color,
+                Icon = icon
+            };
+
+            _context.Priorities.Add(priority);
+            await _context.SaveChangesAsync();
+
+            return Response<int>.Success(priority.Id);
+        }
     }
 }
--- Application/Priorities/Commands/CreatePriority/CreatePriorityCommandValidator.cs
+++ Application/Priorities/Commands/CreatePriority/CreatePriorityCommandValidator.cs
@@ -1,19 +1,51 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.Common.Extensions;
+using WhatBug.Application.Common.Interfaces;
 
 namespace WhatBug.Application.Priorities.Commands.CreatePriority
 {
     public class CreatePriorityCommandValidator : AbstractValidator<CreatePriorityCommand>
     {
-        public CreatePriorityCommandValidator()
+        private IWhatBugDbContext _context;
+
+        public CreatePriorityCommandValidator(IWhatBugDbContext context)
         {
-            RuleFor(v => v.Name).NotEmpty();
-            RuleFor(v => v.ColorId).NotEmpty();
-            RuleFor(v => v.IconId).NotEmpty();
+            _context = context;
+
+            RuleFor(v => v.Name)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("Priority name cannot be empty")
+                .MustAsync(HaveUniqueName).WithMessage(cmd => $"A priority with the name {cmd.Name} already exists");
+
+            RuleFor(v => v.ColorId)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithException(cmd => new ArgumentException(nameof(cmd.ColorId)))
+                .MustAsync(ColorExist).WithException(cmd => new RecordNotFoundException());
+
+            RuleFor(v => v.IconId)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithException(cmd => new ArgumentException(nameof(cmd.IconId)))
+                .MustAsync(IconExist).WithException(cmd => new RecordNotFoundException());
+        }
+
+        public async Task<bool> HaveUniqueName(CreatePriorityCommand command, string name, CancellationToken cancellationToken)
+        {
+            return !await _context.Priorities.AnyAsync(r => r.Name == name);
+        }
+
+        public async Task<bool> ColorExist(CreatePriorityCommand command, int colorId, CancellationToken cancellationToken)
+        {
+            return await _context.Colors.AnyAsync(c => c.Id == colorId);
+        }
+
+        public async Task<bool> IconExist(CreatePriorityCommand command, int iconId, CancellationToken cancellationToken)
+        {
+            return await _context.Icons.AnyAsync(i => i.Id == iconId);
         }
     }
 }
