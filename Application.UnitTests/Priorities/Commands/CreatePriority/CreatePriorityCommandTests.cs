c09f6aae90087b45e221aebfcbc4df1a45f94370
Anys <Anys.I.Sido@gmail.com>
2021-10-21 19:33:32 +0100

    WBUG-50 Add tests

---
+++ Application.UnitTests/Priorities/Commands/CreatePriority/CreatePriorityCommandTests.cs
@@ -0,0 +1,70 @@
+﻿using Application.UnitTests.Common;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Priorities.Commands.CreatePriority;
+using WhatBug.Domain.Entities;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Commands.CreatePriority
+{
+    public class CreatePriorityCommandTests : CommandTestBase
+    {
+        public CreatePriorityCommandTests()
+        {
+            using (var context = CreateContext())
+            {
+                context.Colors.AddRange(new[]
+                {
+                    new Color { Id = 1, Name = "Color1" },
+                    new Color { Id = 2, Name = "Color2" },
+                });
+
+                context.Icons.Add(new Icon { Id = 1, Name = "Icon1", WebName = "icon1" });
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_CreatesPriority()
+        {
+            // Arrange
+            var sut = new CreatePriorityCommandHandler(_context);
+            var command = new CreatePriorityCommand { Name = "NewName", Description = "NewDesc", IconId = 1, ColorId = 1 };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var priority = _context.Priorities.FirstOrDefault();
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            result.Result.ShouldBe(priority.Id);
+            priority.ShouldNotBeNull();
+            priority.Id.ShouldNotBe(default(int));
+            priority.Name.ShouldBe("NewName");
+            priority.Description.ShouldBe("NewDesc");
+            priority.IconId.ShouldBe(1);
+            priority.ColorId.ShouldBe(1);
+            priority.Order.ShouldBe(1);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task Handle_GivenNullOrEmptyDescription_SetsNullDescription(string description)
+        {
+            // Arrange
+            var sut = new CreatePriorityCommandHandler(_context);
+            var command = new CreatePriorityCommand { Name = "NewName", Description = description, IconId = 1, ColorId = 1 };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var priority = _context.Priorities.FirstOrDefault();
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            priority.Description.ShouldBe(null);
+        }
+    }
+}
---
+++ Application.UnitTests/Priorities/Commands/CreatePriority/CreatePriorityCommandValidatorTests.cs
@@ -0,0 +1,110 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.Priorities.Commands.CreatePriority;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Commands.CreatePriority
+{
+    [Collection("ValidatorCollection")]
+    public class CreatePriorityCommandValidatorTests
+    {
+        private CreatePriorityCommandValidator _sut;
+
+        public CreatePriorityCommandValidatorTests(ValidatorTestFixture fixture)
+        {
+            _sut = new CreatePriorityCommandValidator(fixture.CreateContext());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void GivenNullOrEmptyName_HasValidationError(string name)
+        {
+            // Arrange
+            var command = new CreatePriorityCommand { Name = name };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(command => command.Name)
+                .WithErrorMessage("Priority name cannot be empty");
+        }
+
+        [Fact]
+        public void Given_DuplicateName_HasValidationError()
+        {
+            // Arrange
+            var command = new CreatePriorityCommand { Name = "Priority1" };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(command => command.Name)
+                .WithErrorMessage("A priority with the name Priority1 already exists");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Given_ZeroOrLowerColorId_HasArgumentException(int id)
+        {
+            // Arrange
+            var command = new CreatePriorityCommand { ColorId = id };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.ColorId, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void Given_InvalidColorId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var command = new CreatePriorityCommand { ColorId = 4 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.ColorId, typeof(RecordNotFoundException));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Given_ZeroOrLowerIconId_HasArgumentException(int id)
+        {
+            // Arrange
+            var command = new CreatePriorityCommand { IconId = id };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.IconId, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void Given_InvalidIconId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var command = new CreatePriorityCommand { IconId = 4 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.IconId, typeof(RecordNotFoundException));
+        }
+    }
+}
