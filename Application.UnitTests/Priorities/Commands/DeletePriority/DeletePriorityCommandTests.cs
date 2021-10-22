679ba8740c26e0deec8d8f524728fe4289eb2f92
Anys <Anys.I.Sido@gmail.com>
2021-10-22 13:03:23 +0100

    WBUG-50 Add Tests

---
+++ Application.UnitTests/Priorities/Commands/DeletePriority/DeletePriorityCommandTests.cs
@@ -0,0 +1,66 @@
+﻿using Application.UnitTests.Common;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Priorities.Commands.DeletePriority;
+using WhatBug.Domain.Entities;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Commands.DeletePriority
+{
+    public class DeletePriorityCommandTests : CommandTestBase
+    {
+        public DeletePriorityCommandTests()
+        {
+            using (var context = CreateContext())
+            {
+                context.Priorities.AddRange(new[]
+                {
+                    new Priority { Id = 1, IsDefault = true },
+                    new Priority { Id = 2 },
+                });
+
+                context.Issues.Add(new Issue { Id = "DEMO-1", PriorityId = 2 });
+
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_DeletesPriority()
+        {
+            // Arrange
+            var sut = new DeletePriorityCommandHandler(_context);
+            var command = new DeletePriorityCommand { PriorityId = 2 };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var priorities = await _context.Priorities.ToListAsync();
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            priorities.Count.ShouldBe(1);
+            priorities.First().Id.ShouldBe(1);
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_ReassignsIssuesToDefaultPriority()
+        {
+            // Arrange
+            var sut = new DeletePriorityCommandHandler(_context);
+            var command = new DeletePriorityCommand { PriorityId = 2 };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var project = _context.Issues.First(p => p.Id == "DEMO-1");
+
+            // Assert
+            project.PriorityId.ShouldBe(1);
+        }
+    }
+}
---
+++ Application.UnitTests/Priorities/Commands/DeletePriority/DeletePriorityCommandValidatorTests.cs
@@ -0,0 +1,74 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.Priorities.Commands.DeletePriority;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.Priorities.Commands.DeletePriority
+{
+    [Collection("ValidatorCollection")]
+    public class DeletePriorityCommandValidatorTests
+    {
+        private DeletePriorityCommandValidator _sut;
+
+        public DeletePriorityCommandValidatorTests(ValidatorTestFixture fixture)
+        {
+            _sut = new DeletePriorityCommandValidator(fixture.CreateContext());
+        }
+
+        [Fact]
+        public void Given_ValidRequest_HasNoValidationErrors()
+        {
+            // Arrange
+            var command = new DeletePriorityCommand { PriorityId = 2 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Given_ZeroOrLowerPrioirityId_HasArgumentException(int id)
+        {
+            // Arrange
+            var command = new DeletePriorityCommand { PriorityId = id };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.PriorityId, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void Given_InvalidPriorityId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var command = new DeletePriorityCommand { PriorityId = 5 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.PriorityId, typeof(RecordNotFoundException));
+        }
+
+        [Fact]
+        public void Given_IdOfDefaultPriority_HasArgumentException()
+        {
+            // Arrange
+            var command = new DeletePriorityCommand { PriorityId = 4 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.PriorityId, typeof(ArgumentException));
+        }
+    }
+}
