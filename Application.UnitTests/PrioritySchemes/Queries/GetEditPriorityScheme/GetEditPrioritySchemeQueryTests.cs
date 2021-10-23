8c42451978b9d0de8bb4335db3d4996290625409
Anys <Anys.I.Sido@gmail.com>
2021-10-23 19:06:53 +0100

    WBUG-51 Add tests

---
+++ Application.UnitTests/PrioritySchemes/Queries/GetEditPriorityScheme/GetEditPrioritySchemeQueryTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.PrioritySchemes.Queries.GetEditPriorityScheme;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.PrioritySchemes.Queries.GetEditPriorityScheme
+{
+    [Collection("QueryCollection")]
+    public class GetEditPrioritySchemeQueryTests
+    {
+        private readonly IWhatBugDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetEditPrioritySchemeQueryTests(QueryTestFixture fixture)
+        {
+            _context = fixture.CreateContext();
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_ReturnsPriorityScheme()
+        {
+            // Arrange
+            var sut = new GetEditPrioritySchemeQueryHandler(_context, _mapper);
+
+            // Act
+            var result = await sut.Handle(new GetEditPrioritySchemeQuery { Id = 1 }, CancellationToken.None);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            result.Result.Id.ShouldBe(1);
+            result.Result.Name.ShouldBe("PriorityScheme1");
+            result.Result.Description.ShouldBe("PrioritySchemeDesc1");
+            result.Result.PriorityIds.Count.ShouldBe(3);
+            result.Result.PriorityIds.ToArray().ShouldBeEquivalentTo(new[] { 1, 2, 4 });
+            result.Result.Priorities.Count.ShouldBe(4);
+            result.Result.Priorities.Select(p => p.Id).OrderBy(p => p).ToArray().ShouldBeEquivalentTo(new[] { 1, 2, 3, 4 });
+        }
+    }
+}
---
+++ Application.UnitTests/PrioritySchemes/Queries/GetEditPriorityScheme/GetEditPrioritySchemeQueryValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.PrioritySchemes.Queries.GetEditPriorityScheme;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.PrioritySchemes.Queries.GetEditPriorityScheme
+{
+    [Collection("ValidatorCollection")]
+    public class GetEditPrioritySchemeQueryValidatorTests
+    {
+        private readonly GetEditPrioritySchemeQueryValidator _sut;
+
+        public GetEditPrioritySchemeQueryValidatorTests(ValidatorTestFixture fixture)
+        {
+            _sut = new GetEditPrioritySchemeQueryValidator(fixture.CreateContext());
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public async void Given_ZeroOrLowerSchemeId_HasArgumentException(int id)
+        {
+            // Arrange
+            var query = new GetEditPrioritySchemeQuery { Id = id };
+
+            // Act
+            var result = await _sut.TestValidateAsync(query);
+
+            // Assert
+            result.ShouldHaveExceptionFor(query => query.Id, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public async void Given_InvalidSchemeId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var query = new GetEditPrioritySchemeQuery { Id = 4 };
+
+            // Act
+            var result = await _sut.TestValidateAsync(query);
+
+            // Assert
+            result.ShouldHaveExceptionFor(query => query.Id, typeof(RecordNotFoundException));
+        }
+    }
+}
