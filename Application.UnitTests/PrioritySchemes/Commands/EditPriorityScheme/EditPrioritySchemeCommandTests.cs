203f78d209fdd9df1a2baf59472e82b90cf72e9a
Anys <Anys.I.Sido@gmail.com>
2021-10-24 14:02:25 +0100

    WBUG-51 Add tests

---
+++ Application.UnitTests/PrioritySchemes/Commands/EditPriorityScheme/EditPrioritySchemeCommandTests.cs
@@ -0,0 +1,114 @@
+﻿using Application.UnitTests.Common;
+using Shouldly;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.PrioritySchemes.Commands.EditPriorityScheme;
+using WhatBug.Domain.Entities;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.PrioritySchemes.Commands.EditPriorityScheme
+{
+    public class EditPrioritySchemeCommandTests : CommandTestBase
+    {
+        public EditPrioritySchemeCommandTests()
+        {
+            using (var context = CreateContext())
+            {
+                context.Priorities.AddRange(new[]
+                {
+                    new Priority { Id = 1, Name = "Prio1" },
+                    new Priority { Id = 2, Name = "Prio2" },
+                    new Priority { Id = 3, Name = "Prio3", IsDefault = true },
+                });
+
+                context.PrioritySchemes.AddRange(new[]
+                {
+                    new PriorityScheme { Id = 1, Name = "PriorityScheme1", Description = "PriorityScheme1" }
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_UpdatesPriorityScheme()
+        {
+            // Arrange
+            var sut = new EditPrioritySchemeCommandHandler(_context);
+            var command = new EditPrioritySchemeCommand
+            {
+                Id = 1,
+                Name = "TestName",
+                Description = "TestDesc",
+                PriorityIds = new[] { 1, 3 }
+            };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var scheme = _context.PrioritySchemes.First(s => s.Id == 1);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            scheme.Name.ShouldBe("TestName");
+            scheme.Description.ShouldBe("TestDesc");
+            scheme.Priorities.Count.ShouldBe(2);
+            scheme.Priorities.Select(p => p.PriorityId).ShouldContain(1);
+            scheme.Priorities.Select(p => p.PriorityId).ShouldContain(3);
+        }
+
+        [Fact]
+        public async Task Handle_GivenNoPriorities_AssignsDefaultPriority()
+        {
+            // Arrange
+            var sut = new EditPrioritySchemeCommandHandler(_context);
+            var command = new EditPrioritySchemeCommand
+            {
+                Id = 1,
+                Name = "TestName",
+                Description = "TestDesc",
+            };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var scheme = _context.PrioritySchemes.First(s => s.Id == 1);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            scheme.Priorities.Count.ShouldBe(1);
+            scheme.Priorities.First().PriorityId.ShouldBe(3);
+        }
+
+        [Fact]
+        public async Task Handle_GivenNewPriorities_ReplacesOldPriorities()
+        {
+            // Arrange
+            using (var context = CreateContext())
+            {
+                context.PrioritySchemePriorities.AddRange(new[]
+                {
+                    new PrioritySchemePriority { PrioritySchemeId = 1, PriorityId = 1 },
+                    new PrioritySchemePriority { PrioritySchemeId = 1, PriorityId = 3 },
+                });
+            }
+
+            var sut = new EditPrioritySchemeCommandHandler(_context);
+            var command = new EditPrioritySchemeCommand
+            {
+                Id = 1,
+                Name = "TestName",
+                Description = "TestDesc",
+                PriorityIds = new[] { 2 }
+            };
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+            var scheme = _context.PrioritySchemes.First(s => s.Id == 1);
+
+            // Assert
+            result.Succeeded.ShouldBe(true);
+            scheme.Priorities.Count.ShouldBe(1);
+            scheme.Priorities.First().PriorityId.ShouldBe(2);
+        }
+    }
+}
---
+++ Application.UnitTests/PrioritySchemes/Commands/EditPriorityScheme/EditPrioritySchemeCommandValidatorTests.cs
@@ -0,0 +1,127 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.PrioritySchemes.Commands.EditPriorityScheme;
+using WhatBug.Application.UnitTests.Common;
+using Xunit;
+
+namespace WhatBug.Application.UnitTests.PrioritySchemes.Commands.EditPriorityScheme
+{
+    [Collection("ValidatorCollection")]
+    public class EditPrioritySchemeCommandValidatorTests
+    {
+        private EditPrioritySchemeCommandValidator _sut;
+
+        public EditPrioritySchemeCommandValidatorTests(ValidatorTestFixture fixture)
+        {
+            _sut = new EditPrioritySchemeCommandValidator(fixture.CreateContext());
+        }
+
+        [Fact]
+        public void Given_ValidRequest_PassesValidation()
+        {
+            // Arrange
+            var command = new EditPrioritySchemeCommand { Id = 1, Name = "NewName", Description = "NewDesc", PriorityIds = new[] { 1, 2 } };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void Given_ZeroOrLowerSchemeId_HasArgumentException(int id)
+        {
+            // Arrange
+            var command = new EditPrioritySchemeCommand { Id = id };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.Id, typeof(ArgumentException));
+        }
+
+        [Fact]
+        public void Given_InvalidSchemeId_HasRecordNotFoundException()
+        {
+            // Arrange
+            var command = new EditPrioritySchemeCommand { Id = 4 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.Id, typeof(RecordNotFoundException));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void GivenNullOrEmptyName_HasValidationError(string name)
+        {
+            // Arrange
+            var command = new EditPrioritySchemeCommand { Name = name };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(command => command.Name)
+                .WithErrorMessage("Priority scheme name cannot be empty");
+        }
+
+        [Fact]
+        public void Given_DuplicateName_HasValidationError()
+        {
+            // Arrange
+            var command = new EditPrioritySchemeCommand { Id = 3, Name = "PriorityScheme1" };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(command => command.Name)
+                .WithErrorMessage("A priority scheme with the name PriorityScheme1 already exists");
+        }
+
+        [Fact]
+        public void Given_NullPriorityIds_HasArgumentException()
+        {
+            // Arrange
+            var command = new EditPrioritySchemeCommand { Id = 3 };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.PriorityIds, typeof(ArgumentException));
+        }
+
+        [Theory]
+        [InlineData(1, 2, 9)]
+        [InlineData(4, 5, 7)]
+        public void Given_AnyInvalidPriorityId_HasRecordNotFoundException(int priorityId1, int priorityId2, int priorityId3)
+        {
+            // Arrange
+            var command = new EditPrioritySchemeCommand
+            {
+                Id = 1,
+                PriorityIds = new int[] { priorityId1, priorityId2, priorityId3 }
+            };
+
+            // Act
+            var result = _sut.TestValidate(command);
+
+            // Assert
+            result.ShouldHaveExceptionFor(command => command.PriorityIds, typeof(RecordNotFoundException));
+        }
+    }
+}
