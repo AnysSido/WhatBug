99bf13dbe1467aee2249f9e02a73b0679cc680a4
Anys <Anys.I.Sido@gmail.com>
2021-10-24 15:12:49 +0100

    WBUG-51 Refactor create priority scheme feature query

--- Application/PrioritySchemes/Queries/GetCreatePriorityScheme/CreatePrioritySchemeDTO.cs
+++ Application/PrioritySchemes/Queries/GetCreatePriorityScheme/CreatePrioritySchemeDTO.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using WhatBug.Common.Mapping;
+using WhatBug.Domain.Entities;
 
 namespace WhatBug.Application.PrioritySchemes.Queries.GetCreatePriorityScheme
 {
@@ -6,6 +8,15 @@ namespace WhatBug.Application.PrioritySchemes.Queries.GetCreatePriorityScheme
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public IList<int> PriorityIds { get; set; }
         public IList<PriorityDTO> Priorities { get; set; }
+    }
+
+    public class PriorityDTO : IMapFrom<Priority>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconWebName { get; set; }
+        public string ColorName { get; set; }
     }
 }
--- Application/PrioritySchemes/Queries/GetCreatePriorityScheme/GetCreatePrioritySchemeQuery.cs
+++ Application/PrioritySchemes/Queries/GetCreatePriorityScheme/GetCreatePrioritySchemeQuery.cs
@@ -1,13 +1,43 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Common.MediatR;
+using WhatBug.Application.Common.Security;
+using WhatBug.Domain.Data;
 
 namespace WhatBug.Application.PrioritySchemes.Queries.GetCreatePriorityScheme
 {
-    public class GetCreatePrioritySchemeQuery : IRequest<CreatePrioritySchemeDTO>
+    [Authorize(Permissions.ManagePrioritySchemes)]
+    public class GetCreatePrioritySchemeQuery : IQuery<Response<CreatePrioritySchemeDTO>>
     {
+    }
+
+    public class GetCreatePrioritySchemeQueryHandler : IRequestHandler<GetCreatePrioritySchemeQuery, Response<CreatePrioritySchemeDTO>>
+    {
+        private readonly IWhatBugDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetCreatePrioritySchemeQueryHandler(IWhatBugDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<CreatePrioritySchemeDTO>> Handle(GetCreatePrioritySchemeQuery request, CancellationToken cancellationToken)
+        {
+            var dto = new CreatePrioritySchemeDTO
+            {
+                Priorities = await _context.Priorities
+                    .OrderBy(p => p.Order)
+                    .ProjectTo<PriorityDTO>(_mapper.ConfigurationProvider).ToListAsync()
+            };
+
+            return Response<CreatePrioritySchemeDTO>.Success(dto);
+        }
     }
 }
