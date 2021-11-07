8102cb07369df5e3891b4467e2b3d5d5e7314af7
Anys <Anys.I.Sido@gmail.com>
2021-11-07 15:45:49 +0000

    WBUG-54 Add DemoModeBehavior that short-circuits the MediatR request pipeline for read-only users before a command can execute

---
+++ Application/Common/Behaviors/DemoModeBehavior.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Common.MediatR;
+
+namespace WhatBug.Application.Common.Behaviors
+{
+    public class DemoModeBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand<TResponse>
+        where TResponse : Response
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public DemoModeBehavior(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_currentUserService.IsReadOnly)
+            {
+                var responseType = typeof(TResponse);
+
+                if (responseType.IsGenericType)
+                    return Response<int>.Success(1) as TResponse;
+
+                return Response.Success() as TResponse;
+            }
+
+            return await next();
+        }
+    }
+}
