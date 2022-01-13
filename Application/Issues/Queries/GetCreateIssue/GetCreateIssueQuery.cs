6c2610bd49d0706bbe5a2f1ee80480ea33d28344
Anys <Anys.I.Sido@gmail.com>
2022-01-13 12:17:59 +0000

    WBUG-52 Finalize the Create Issue window

--- Application/Issues/Queries/GetCreateIssue/GetCreateIssueQuery.cs
+++ Application/Issues/Queries/GetCreateIssue/GetCreateIssueQuery.cs
@@ -1,9 +1,83 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WhatBug.Application.Authorization;
+using WhatBug.Application.Common.Exceptions;
+using WhatBug.Application.Common.Interfaces;
+using WhatBug.Application.Common.MediatR;
+using WhatBug.Domain.Data;
 
 namespace WhatBug.Application.Issues.Queries.GetCreateIssue
 {
-    public class GetCreateIssueQuery :IRequest<CreateIssueDTO>
+    public record GetCreateIssueQuery : IQuery<Response<GetCreateIssueQueryResult>>
     {
         public int? ProjectId { get; set; }
+    }
+
+    public class GetCreateIssueQueryHandler : IRequestHandler<GetCreateIssueQuery, Response<GetCreateIssueQueryResult>>
+    {
+        private readonly IWhatBugDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IAuthorizationManager _authManager;
+
+        public GetCreateIssueQueryHandler(IWhatBugDbContext context, IMapper mapper, IAuthorizationManager authManager)
+        {
+            _context = context;
+            _mapper = mapper;
+            _authManager = authManager;
+        }
+
+        public async Task<Response<GetCreateIssueQueryResult>> Handle(GetCreateIssueQuery request, CancellationToken cancellationToken)
+        {
+            var projectSummaries = await GetProjectsAsync();
+
+            if (projectSummaries.Count == 0)
+                throw new AccessDeniedException("No access to any projects.");
+
+            var project = await _mapper.ProjectTo<ProjectDTO>(_context.Projects)
+                .FirstOrDefaultAsync(p => p.Id == request.ProjectId.GetValueOrDefault(projectSummaries.First().Id));
+
+            if (project == null)
+                throw new AccessDeniedException();
+
+            var projectUsers = await _context.ProjectRoleUsers
+                .Where(p => p.ProjectId == project.Id)
+                .Select(u => u.User)
+                .ProjectTo<UserDTO>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            projectUsers = projectUsers
+                .GroupBy(u => u.Id)
+                .Select(group => group.First())
+                .ToList();
+
+            var issueTypes = await _mapper.ProjectTo<IssueTypeDTO>(_context.IssueTypes).ToListAsync();
+
+            var dto = new GetCreateIssueQueryResult
+            {
+                Project = project,
+                Projects = projectSummaries,
+                IssueTypes = issueTypes,
+                AssignableUsers = projectUsers,
+                ReportingUsers = projectUsers
+            };
+
+            return Response<GetCreateIssueQueryResult>.Success(dto);
+        }
+
+        private async Task<List<ProjectSummaryDTO>> GetProjectsAsync()
+        {
+            if (await _authManager.HasPermission(Permissions.ViewAllProjects))
+                return await _mapper.ProjectTo<ProjectSummaryDTO>(_context.Projects).ToListAsync();
+
+            var projects = await _authManager.GetProjectsWithPermissionAsync(Permissions.ViewProject);
+
+            return await _context.Projects.Where(p => projects.Contains(p.Id)).ProjectTo<ProjectSummaryDTO>(_mapper.ConfigurationProvider).ToListAsync();
+        }
     }
 }
---
+++ Application/Issues/Queries/GetCreateIssue/GetCreateIssueQueryResult.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using WhatBug.Common.Mapping;
+using WhatBug.Domain.Entities;
+
+namespace WhatBug.Application.Issues.Queries.GetCreateIssue
+{
+    public class GetCreateIssueQueryResult
+    {
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public ProjectDTO Project { get; set; }
+        public IList<ProjectSummaryDTO> Projects { get; set; }
+        public IList<IssueTypeDTO> IssueTypes { get; set; }
+        public IList<UserDTO> AssignableUsers { get; set; }
+        public IList<UserDTO> ReportingUsers { get; set; }
+    }
+
+    public class IssueTypeDTO : IMapFrom<IssueType>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ColorName { get; set; }
+        public string IconName { get; set; }
+        public string IconWebName { get; set; }
+    }
+
+    public class PriorityDTO : IMapFrom<Priority>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ColorName { get; set; }
+        public string IconName { get; set; }
+        public string IconWebName { get; set; }
+    }
+
+    public class ProjectDTO : IMapFrom<Project>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<PriorityDTO> Priorities { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Project, ProjectDTO>()
+                .ForMember(d => d.Priorities, opt => opt.MapFrom(s => s.PriorityScheme.Priorities.Select(p => p.Priority).OrderBy(p => p.Order)));
+        }
+    }
+
+    public class ProjectSummaryDTO : IMapFrom<Project>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UserDTO : IMapFrom<User>
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
