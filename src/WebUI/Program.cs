3e008b94bb4e8573f6649d82b2dd49c580144210
Anys Sido <anys.dev@gmail.com>
2022-01-31 09:22:26 +0000

    WBUG-73 Add build automation (#3)
    Update MSBuild, add docker and docker-compose with postgres included for build automation. Tests run on image build.
    
    Migrate databases on startup.
    
    Create Jenkinsfile for automated image build and publish.

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,19 +1,47 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WhatBug.Infrastructure.Identity;
+using WhatBug.Persistence;
 
 namespace WebUI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<WhatBugDbContext>();
+                    var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+
+                    context.Database.Migrate();
+                    identityContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+
+                    throw;
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
